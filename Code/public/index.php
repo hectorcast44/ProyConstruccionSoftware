7c8c7a522247a839ac135b256652a4c8d5f29d7c
Joseph Antonio García Avilés <A23216420@alumnos.uady.mx>
2025-12-02 19:40:52 -0600

    issue Replace require_once with namespace import mechanism through the use keyword resolved

--- Code/public/index.php
+++ Code/public/index.php
@@ -4,23 +4,7 @@ use App\Core\Router;
 
 session_start();
 
-// Autoloader
-spl_autoload_register(function ($class) {
-    $prefix = 'App\\';
-    $base_dir = __DIR__ . '/../app/';
-
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    if (file_exists($file)) {
-        require_once $file;
-    }
-});
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $router = new Router();
 
